fe9a36fdff49a7abc3b5db22aaac4e6ae17649e1
SadikRahman14 <sadik.nai.008@gmail.com>
2023-12-06 22:54:55 +0600

    Two Base Button of Home Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -14,51 +16,150 @@ class MyApp extends StatelessWidget{
         primarySwatch: Colors.blue,
 
       ),
-      home: ProductDetailsPage(),
+      home: HomePage(),
     );
   }
 }
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-class ProductDetailsPage extends StatelessWidget{
   @override
-  Widget build(BuildContext context){
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
-        title: Center(child: Text('Football Options')),
-      ),
+      backgroundColor: Colors.indigo[200],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              // Greetings Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
 
 
-      body:
-        Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       // Hi Khairul
+                       Text(
+                           'Hi Khairul',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+
+                            ),
+                       ),
+                       // Date
+                       Text(
+                         '10 December, 2023',
+                         style: TextStyle(
+                           color: Colors.black,
+                         ),
+
+                       )
+                     ],
+                   ),
+               //Notifications
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.all(4),
+                  child: Icon(
+                      Icons.notifications,
+                      color: Colors.black87,
+                  ),
+                ),
+
+              ],),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Search',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+
+              ),
+              // Base Container
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(12),
+                height: 200, width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[400],
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Code to execute when the first button is pressed
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.brown[900]),
+                      fixedSize: MaterialStateProperty.all(Size(300, 80)),
+                    ),
+                    child: Text(
+                        'DONATE NOW',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                        ),
+                    ),
+
+                  ),
+                  SizedBox(height: 16), // Adding some space between buttons
+                  ElevatedButton(
+
+                    onPressed: () {
+                      // Code to execute when the first button is pressed
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.brown[900]),
+                      fixedSize: MaterialStateProperty.all(Size(300, 80)),
+                    ),
+                    child: Text(
+                      'REQUEST FOR BLOOD',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+              ,
+              )
+
+            ],
           ),
-
-
         ),
-      bottomNavigationBar: BottomNavigationBar(
-        //currentIndex: _currentIndex,
-        onTap: (index) {
-
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
-          ),
-        ],
       ),
     );
-
   }
 }
