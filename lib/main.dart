ffbc1c1f66cdda928f1adf89907819ba971dc37b
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2023-12-06 21:31:21 +0600

    basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget{
       title: 'Product App',
       theme : ThemeData(
         primarySwatch: Colors.blue,
-
       ),
       home: ProductDetailsPage(),
     );
@@ -23,42 +22,53 @@ class ProductDetailsPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
-        title: Center(child: Text('Football Options')),
-      ),
+      backgroundColor: Colors.white, //deepPurple[900]
+      // appBar: AppBar(
+      //   backgroundColor: Colors.amberAccent,
+      //   title: Center(child: Text('GG')),
+      // ),
 
 
-      body:
-        Center(
+      body: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 320,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-
-
         ),
-      bottomNavigationBar: BottomNavigationBar(
-        //currentIndex: _currentIndex,
-        onTap: (index) {
 
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
-          ),
-        ],
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   //currentIndex: _currentIndex,
+      //   onTap: (index) {},
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.search),
+      //       label: 'Search',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.person),
+      //       label: '',
+      //     ),
+      //   ],
+      // ),
     );
-
   }
 }
